1e9699e10a2dc4a9756fec512f0762c3f5d7f5c9
JonLz <jon.lazar@gmail.com>
2012-10-24 14:09:06 -0400

    Update user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,71 +1,75 @@
 require 'spec_helper'
 
-  describe User do
-  before { @user = User.new(name: "Test User", password: "abc123", password_confirmation: "abc123") }
-    
-  subject { @user }
+ describe User do 
+	before { @user = User.new(name: 'Jojo', 
+														password: '123456',
+														password_confirmation:'123456')}
+	subject {@user}
 
-  it { should respond_to(:name) }
-  it { should respond_to(:password_digest) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
-  it { should respond_to(:bankroll) }
-  
-  it { should be_valid }
+	it { should respond_to(:name)}
+	it { should respond_to(:password)}
+	it { should respond_to(:password_confirmation)}
+	it { should respond_to(:bankroll)}
+	it { should respond_to(:password_digest)}
 
-  describe "when no name is entered" do
-    before { @user.name = " " }
-    it { should_not be_valid }
-  end
-  
-  describe "when name is too long" do
-    before { @user.name = "t" * 26 }
-    it { should_not be_valid }
-  end
-  
-  describe "when name already exists" do
-    before do
-      user_with_same_name = @user.dup
-      user_with_same_name.name = @user.name.upcase
-      user_with_same_name.save
-    end
-    it { should_not be_valid }
-  end
-  
-  describe "with no password entered" do
-    before { @user.password = @user.password_confirmation = " " }
-    it { should_not be_valid }
-  end
+	it { should be_valid}
 
-  describe "when password doesn't match confirmation" do
-    before { @user.password_confirmation = "mismatch" }
-    it { should_not be_valid }
-  end
+	describe "no name entered" do
+		before { @user.name = ''}
+		it { should_not be_valid}
+	end
 
-  describe "when password confirmation is nil" do
-  before { @user.password_confirmation = nil }
-    it { should_not be_valid }
-  end
-  
-  describe "with a password that's too short" do
-    before { @user.password = @user.password_confirmation = "a" * 5 }
-    it { should be_invalid }
-  end
-  
-  describe "return value of authenticate method" do
-    before { @user.save }
-    let(:found_user) { User.find_by_name(@user.name) }
-    
-  describe "with valid password" do
-    it { should == found_user.authenticate(@user.password) }
-  end
+	describe "name is too long" do
+		before { @user.name = 'kljg53'*5}
+		it { should_not be_valid}
+	end
 
-  describe "with invalid password" do
-    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+ describe "name is not unique" do
+ 	  before do 
+ 	  	@dupe_user = @user.dup
+ 	  	@dupe_user.save
+ 	  end
+ 	  it { should_not be_valid}
+ 	end
 
-    it { should_not == user_for_invalid_password }
-      specify { user_for_invalid_password.should be_false }
-    end
-    
+ describe "password too short" do
+ 	before do 
+ 		@user.password = '12345'
+ 		@user.password_confirmation = '12345'
+ 	end
+ 	it { should_not be_valid}
  end
-end
+
+ describe "no password" do
+ 	before { @user.password = ''}
+ 	it { should_not be_valid}
+ end
+
+ describe "no password confirm" do
+ 	before { @user.password_confirmation = ''}
+ 	it { should_not be_valid}
+ end
+
+ describe "password mismatch" do
+ 	before do
+ 	 @user.password_confirmation = '1234567'
+ 	end
+ 	it { should_not be_valid}
+ end
+
+ describe "test authenticate" do
+ 	before { @user.save }
+ 	let(:found_user) { User.find_by_name(@user.name) }
+
+ 	describe "with valid password" do
+ 		it {should == found_user.authenticate(@user.password)}
+ 	end
+
+ 	describe "with invalid password" do
+ 		let(:invalid_pass_user) { found_user.authenticate('Invalid')}
+ 		it {should_not == invalid_pass_user}
+ 		specify {invalid_pass_user.should be_false}
+ 	end
+ end
+
+ end
