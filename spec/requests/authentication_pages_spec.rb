38b5b2b74b6d7749548212bdbb9efdcedc1c205c
JonLz <jon.lazar@gmail.com>
2012-10-25 10:48:48 -0400

    Update user login tests

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,34 +1,35 @@
 require 'spec_helper'
 
-describe "UserLogins" do
-  subject { page }
+describe 'LoginPages' do
+subject { page }
 
-  describe "login page" do
-    before { visit login_path }
-
-    it { should have_selector('h1',    text: 'Sign in') }
+  describe 'the page should say sign in' do 
+    before { visit login_path } 
+    it { should have_selector('h1', text:'Sign in') }
   end
-  
-  describe "signin" do
+
+  describe 'user signup' do
     before { visit login_path }
 
-    describe "with invalid information" do
-      before { click_button "Sign in" }
+    describe 'with invalid information' do
+      before { click_button 'Sign in' }
 
-      it { should have_selector('h1', text: 'Sign in') }
-      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      it { should have_selector('h1', text:'Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid email/password combination')}
     end
-    
-     describe "with valid information" do
+
+    describe 'with valid information' do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Name",    with: user.name
-        fill_in "Password", with: user.password
-        click_button "Sign in"
+        fill_in "Name",     with:  user.name
+        fill_in "Password", with:  user.password
+        click_button 'Sign in' 
       end
-      
-      it { should have_selector('h1', text: 'Sign Up For BlackJack') }
+
+      it { should have_selector('h1', text:'Sign Up For BlackJack') }
       it { should_not have_selector('div.alert.alert-error') }
     end
-  end
-end
+  
+  end #end describe user signup
+
+end #end describe Loginpages
